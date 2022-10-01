643ff10474f75af8bc35c80910f2dfa8693086f3
derkius <derkius1@gmail.com>
2022-10-01 18:07:50 +0200

    Trying to return the total change of the coins inserted.

--- Intermediate-projects/Coffee Machine/functions.py
+++ Intermediate-projects/Coffee Machine/functions.py
@@ -1,21 +1,41 @@
-from machine_resources import MENU as mn
+from machine_resources import MENU, resources
 
 
 def coffee_choice(choose):
     if choose == 'espresso':
-        water = mn['espresso']['ingredients']['water']
-        coffee = mn['espresso']['ingredients']['coffee']
-        cost = mn['espresso']['cost']
-        return 'espresso'
+        water = MENU['espresso']['ingredients']['water']
+        coffee = MENU['espresso']['ingredients']['coffee']
+        """cost = MENU['espresso']['cost']"""
+        return coffee_maker(water, coffee)
     elif choose == 'late':
         return 'late'
     elif choose == 'cappuccino':
         return 'cappuccino'
     elif choose == 'report':
-
-        return 'report'
+        return f"Water: {resources['water']}ml \n" \
+               f"Milk: {resources['milk']}ml \n" \
+               f"Coffee: {resources['water']}ml \n" \
+               f"Money: $0"
     else:
         return "Only choose between 'espresso', 'late' or 'cappuccino'."
+
+
+def coffee_maker(water, coffee, milk=0):
+
+    machine_water = resources["water"]
+    machine_milk = resources["milk"]
+    machine_coffee = resources["coffee"]
+
+    if machine_water >= water and machine_milk >= milk and machine_coffee >= coffee:
+        return "Here is your ☕. Enjoy!"
+    elif water > machine_water:
+        return "Sorry, there's not enough water. Money refunded."
+    elif milk > 0 and milk > machine_milk:
+        return "Sorry, there's not enough milk. Money refunded."
+    elif coffee > machine_coffee:
+        return "Sorry, there's not enough coffee. Money refunded."
+    else:
+        return "Sorry, something's wrong. Money refunded."
 
 
 def coins_change(coin, units):
@@ -29,5 +49,9 @@ def coins_change(coin, units):
         return units * 0.01
 
 
-def resources_left(water, coffee, cost, milk=0):
-    return milk
+def total_coins_change(quarters, dimes, nickles, pennies):
+
+    total = quarters + dimes + nickles + pennies
+
+    return total
+
--- Intermediate-projects/Coffee Machine/main.py
+++ Intermediate-projects/Coffee Machine/main.py
@@ -1,6 +1,21 @@
-from functions import coffee_choice
+from functions import coffee_choice, total_coins_change, coins_change
+from machine_resources import MENU, resources
 
 while True:
-    coffee_choose = input("What would you like? (espresso/late/cappuccino): ")
+    """coffee_choose = input("What would you like? (espresso/late/cappuccino): ")
+    """
 
+    print("Please insert coins.")
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickles = int(input("How many nickles? "))
+    pennies = int(input("How many pennies? "))
+
+    print(total_coins_change(coins_change("quarters", quarters), coins_change("dimes", dimes), coins_change("nickles", nickles), coins_change("pennies", pennies)))
+
+"""
     print(coffee_choice(coffee_choose))
+
+    if coffee_choice(coffee_choose) == "☕":
+        resources["water"] -= MENU[coffee_choose]["ingredients"]["water"]
+"""
